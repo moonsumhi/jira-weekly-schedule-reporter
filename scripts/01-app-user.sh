e610696068b2c67272e854b1b968c437248fa5b0
sumee <moonsumhi@gmail.com>
2025-11-06 21:34:04 +0900

    mongo admin 계정 수정

--- scripts/01-app-user.sh
+++ scripts/01-app-user.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "Creating application database and user…"
 mongosh --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin <<'EOF'
-const dbName = process.env.APP_DB_NAME || "appdb";
-const user   = process.env.APP_DB_USER || "appuser";
-const pass   = process.env.APP_DB_PASS || "apppass";
+const dbName = "$APP_DB_NAME" || "appdb";
+const user   = "$APP_DB_USER" || "appuser";
+const pass   = "$APP_DB_PASS" || "apppass";
 
 db.getSiblingDB(dbName).createUser({
   user: user,
